006b15741fe04b27fd1b2980a02e5bf03108f7aa
Ashrafuj Jaman <tanbingithub@.com>
2023-04-07 01:06:13 +0600

    header added

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./components/Home/Home";
+import Main from "./components/Layout/Main";
+import OrderReview from "./components/OrderReview/OrderReview";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('t-shirts.json')
+
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>
+      }
+    ]
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
